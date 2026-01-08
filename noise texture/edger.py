d9b533bec2d43192486de06e8c9517812d297109
Phillip <phildamian@web.de>
2026-01-08 21:51:43 +0100

    caysdasd

--- noise texture/edger.py
+++ noise texture/edger.py
@@ -3,6 +3,7 @@ import numpy as np
 from PIL import Image
 import random
 import time
+import math
 from collections import deque
 import matplotlib.pyplot as plt
 import imageio.v3 as iio
@@ -24,6 +25,16 @@ OPTIONS = [
     (2)  
 ]
 
+CARDINAL_DIRECTIONS = [
+     (1, 0),    # east
+     (1, 1),    # north east
+     (-1, 0),   # west
+     (-1, 1),    # North west
+     (0, 1),    # south
+     (1, 1),    # south East
+     (1, -1),    # South West
+     (0, -1)    # north
+ ]
 def in_bounds(x, y):
     return 0 <= x < SIZE and 0 <= y < SIZE
 
@@ -54,36 +65,27 @@ grid = np.zeros((x_size, y_size), dtype=np.uint8)
 new_x_size = int(x_size/3)
 new_y_size = int(y_size/3)
 
-for x in range(new_x_size):
-    for y in range(new_y_size):
-        pixel = image[x, y]
-        total_red = 0
-        total_blue = 0
-        total_green = 0
-        for i in range(0, 3):
-            for j in range(0, 3):
-                total_red += pixel[0]
-                total_green += pixel[1]
-                total_blue += pixel[2]
+for x in range(x_size):
+    for y in range(y_size):
 
-        total_red /= 9
-        total_green /= 9
-        total_blue /= 9
-        total = (total_red,total_blue,total_red)
+        # Check each neighbouring pixel (8 directional)
+        for dx,dy in CARDINAL_DIRECTIONS:
+            nx, ny = x + dx, y + dy
 
-        for k in range(0, 3):
-            for l in range(0, 3):
-
-                difference = (pixel[0]-total_red,pixel[1]-total_blue,pixel[2]-total_green)
-
-                if abs(pixel[0]-total_red) > 1:
-                    grid[x,y] = 1
-                if abs(pixel[1]-total_blue) > 1:
-                    grid[x,y] = 1
-                if abs(pixel[2]-total_green) > 1:
+            if 0 <= nx < x_size and 0 <= ny < y_size:
+                origin = image[x,y].astype(int)
+                neighbour = image[x+dx,y+dy].astype(int)
+                d_r = origin[0]-neighbour[0]
+                d_b = origin[1]-neighbour[1]
+                d_g = origin[2]-neighbour[2]
+                #calc distance
+                distance = math.sqrt(d_r**2 + d_b**2 + d_g**2)
+                if distance > 75:
                     grid[x,y] = 1
 
-                print("Average in neighbourhood: " + str(total) + " difference: " + str(difference))
+
+
+
 
 
 
@@ -118,9 +120,15 @@ for x in range(new_x_size):
 # Export texture
 # ------------------
 
+h, w = grid.shape
+color_img = np.zeros((h,w,3), dtype=np.uint8)
+
+color_img[grid == 0] = [0, 0, 0]          # 0 = Black (Background)
+color_img[grid == 1] = [255, 255, 255]    # 1 = White (Corridors)
+color_img[grid == 2] = [0, 100, 255]
 
 
-color_img = (grid * 255).astype(np.uint8)
+
 
 # Display result
 plt.imshow(image) # Show original
